d54dd2dd5a4ee668556de2d8dfc293a3b3e0ab80
MOHAMMADMODASSIR <66721527+MOHAMMADMODASSIR@users.noreply.github.com>
2022-06-11 10:47:57 +0530

    First Commit

--- client/src/redux/api.js
+++ client/src/redux/api.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-// const devEnv = process.env.NODE_ENV !== "production";
-
-// const { REACT_APP_DEV_API, REACT_APP_PROD_API } = process.env;
-
 const API = axios.create({
     baseURL: "http://localhost:5000"
-        //     baseURL: `${devEnv ? REACT_APP_DEV_API : REACT_APP_PROD_API}`,
 });
 
 API.interceptors.request.use((req) => {
@@ -34,16 +29,3 @@ export const getToursBySearch = (searchQuery) =>
 export const getTagTours = (tag) => API.get(`/tour/tag/${tag}`);
 export const getRelatedTours = (tags) => API.post(`/tour/relatedTours`, tags);
 export const likeTour = (id) => API.patch(`/tour/like/${id}`);
-
-// export const getTour = (id) => API.get(`/tour/${id}`);
-// export const deleteTour = (id) => API.delete(`/tour/${id}`);
-// export const updateTour = (updatedTourData, id) =>
-//     API.patch(`/tour/${id}`, updatedTourData);
-// export const getToursByUser = (userId) => API.get(`/tour/userTours/${userId}`);
-
-// export const getToursBySearch = (searchQuery) =>
-//     API.get(`/tour/search?searchQuery=${searchQuery}`);
-
-// export const getTagTours = (tag) => API.get(`/tour/tag/${tag}`);
-// export const getRelatedTours = (tags) => API.post(`/tour/relatedTours`, tags);
-// export const likeTour = (id) => API.patch(`/tour/like/${id}`);
